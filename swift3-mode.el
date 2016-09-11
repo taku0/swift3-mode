343d2ab2db0f133cdd2515ebff343ae736b0591e
taku0 <mxxouy6x3m_github@tatapa.org>
2016-09-11 23:41:13 +0900

    Bump version to 2.1

--- swift3-mode.el
+++ swift3-mode.el
@@ -7,7 +7,7 @@
 ;;       Bozhidar Batsov <bozhidar@batsov.com>
 ;;       Arthur Evstifeev <lod@pisem.net>
 ;;
-;; Version: 2.0.1
+;; Version: 2.1
 ;; Package-Requires: ((emacs "24.4"))
 ;; Keywords: languages swift
 ;; URL: https://github.com/taku0/swift3-mode
